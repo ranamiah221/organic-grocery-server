0a09f76920547505aa9d17041d8d899632411790
ranamiah221 <ranarasul21@gmail.com>
2024-12-15 23:59:33 +0600

    added post and get api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -23,6 +23,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    // collection...
+    const productCollection= client.db('organicDB').collection('products')
+    const cartCollection= client.db('organicDB').collection('carts')
+    // get product data from DB
+    app.get('/products', async(req, res)=>{
+      const result = await productCollection.find().toArray()
+      res.send(result)
+    })
+    // get specific product
+    app.get('/products/:id', async(req, res)=>{
+      const id= req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
+    // post carts...
+    app.post('/carts', async(req, res)=>{
+      const cart = req.body;
+      const result = await cartCollection.insertOne(cart)
+      res.send(result)
+    })
+    app.get('/carts', async(req, res)=>{
+      let query ={};
+      if(req.params?.email){
+         query={email: req.params?.email}
+      }
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
